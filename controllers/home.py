1f1945178fbd0742095189507788ec7de39905e3
gaurav sharma <gaurav sharma>
2017-06-18 18:26:25 +0530

    18th june 1st commit

--- controllers/home.py
+++ controllers/home.py
@@ -14,6 +14,15 @@ class notesHandler(tornado.web.RequestHandler):
 	@tornado.gen.coroutine
 	def get(self):
 		notes=yield db.table_notes.find().to_list(None)
+		for n in notes:
+			print n
+			# title=notes[n]['title']
+			# body=notes[n]['body']
+			# body=body.replace("<br/>","\n")
+			# notes={
+			# 	"title":title,
+			# 	"body":body
+			# }
 		self.render('notes.html',notes=notes)
 
 class oslHandler(tornado.web.RequestHandler):
